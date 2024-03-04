fc7349d06c7eab92b2b119c89acc0f37bf32fe28
Eugen Svirbutovich <4workacc@gmail.com>
2024-03-04 15:06:47 +0300

    feat: draggable

--- scripts.js
+++ scripts.js
@@ -1,113 +1,117 @@
-let app = document.getElementById('app');
-let fontSize = 25;
-let arrowHeight = {
-    x: 50,
-    y: 50
-}
-let midCoords = {
-    x: 500,
-    y: 400
-};
-let arrowCoords = [
-    {//0 vertical up
-        x: midCoords.x, 
-        y: 30,
-        x1: midCoords.x-arrowHeight.x,
-        y1: 80,
-        x2: midCoords.x+arrowHeight.x,
-        y2: 80,
-        text: 'text0',
-        textMargin: {
-            x: 0,
-            y: -10
-        },
-    },
-    { //1 vertical dowm
-        x: midCoords.x, 
-        y: 670,
-        x1: midCoords.x-arrowHeight.x,
-        y1: 620,
-        x2: midCoords.x+arrowHeight.x,
-        y2: 620,
-        text: 'text1',
-        textMargin: {
-            x: 0,
-            y: 15
-        }
-    },
-    {
-        // 2 horizontal left =,
-        x: 30,
-        y: midCoords.y,
-        x1: 30+arrowHeight.x,
-        y1: midCoords.y-arrowHeight.y,
-        x2: 30+arrowHeight.x,
-        y2: midCoords.y+arrowHeight.y,
-        text: 'text3',
-        textMargin: {
-            x: 50,
-            y: -15
-        }
-    },
-    {
-        // 3 horizontal right =,
-        x: 970,
-        y: midCoords.y,
-        x1: 970-arrowHeight.x,
-        y1: midCoords.y-arrowHeight.y,
-        x2: 970-arrowHeight.x,
-        y2: midCoords.y+arrowHeight.y,
-        text: 'text4',
-        textMargin: {
-            x: -50,
-            y: -15
-        }
+class AnswerBlock {
+    constructor(x, y, color, word) {
+        this.color = color
+        this.x = x
+        this.y = y
+        this.word = word;
+        this.radius = 10
+        this.selected = false
+
     }
-]
-
-window.onload = function(){   
-    drawArrow(3);    
-    drawArrow(2);
-    drawArrow(0);
-    drawCircle();
-}
-
-function drawCircle(){
-    var canvas = document.getElementById('canvas');
-    if (canvas.getContext){
-        var ctx = canvas.getContext("2d");
-        ctx.beginPath();
-        ctx.fillStyle = "rgb(200,0,0)";
-        ctx.arc(midCoords.x, midCoords.y, 100, 0, 360 * Math.PI, false);
-        ctx.fill();
-        
-        ctx.font = '24pt Calibri';
-        ctx.fillStyle = 'white';
-        ctx.textAlign = 'center';
-        ctx.fillText('Складаныя', midCoords.x, midCoords.y - fontSize+10);
-        ctx.fillText('часціны', midCoords.x, midCoords.y+10);
-        ctx.fillText('мовы', midCoords.x, midCoords.y+fontSize+10);
-    }
-}
-
-function drawArrow(ind){
-    var canvas = document.getElementById('canvas');
-    if (canvas.getContext){
-        var ctx = canvas.getContext("2d");
-        ctx.beginPath();
-        ctx.moveTo(midCoords.x, midCoords.y);        
-        ctx.lineTo(arrowCoords[ind].x, arrowCoords[ind].y);
-        ctx.lineTo(arrowCoords[ind].x1, arrowCoords[ind].y1);
-        ctx.moveTo(arrowCoords[ind].x, arrowCoords[ind].y);
-        ctx.lineTo(arrowCoords[ind].x2, arrowCoords[ind].y2);
-        ctx.stroke();
+    draw(context) {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+        context.fillStyle = this.color;
+        context.fill();
+        context.lineWidth = 5;
+        context.strokeStyle = this.color;
+        context.stroke();
 
         ctx.font = '24pt Calibri';
-        ctx.fillStyle = 'black';
+        ctx.fillStyle = 'red';
         ctx.textAlign = 'center';
-        ctx.fillText('Складаныя', 
-                    arrowCoords[ind].x+arrowCoords[ind].textMargin.x, 
-                    arrowCoords[ind].y+arrowCoords[ind].textMargin.y);
+        ctx.fillText(this.word, this.x, this.y-this.radius);
+    }
+    update() {
+        this.x += 0.1
+    }
+
+    select() {
+        this.selected = !this.selected
+    }
+
+    activate() {
+        this.active = !this.active
     }
 }
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d')
+let w = canvas.width = 1000;
+let h = canvas.height = 700;
+let hsl = (h, s, l) => `hsl(${h},${s}%,${l}%)`
+let selectedAnswer = null;
+
+let wordsArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let answerBlocks = new Array(10).fill().map((el, ind) => new AnswerBlock(50 + ind * 100, 670, '#00FF00', wordsArr[ind]));
+console.log(answerBlocks)
+
+
+let getMouseCoords = (canvas, event) => {
+    let canvasCoords = canvas.getBoundingClientRect()
+    return {
+        x: event.pageX - canvasCoords.left,
+        y: event.pageY - canvasCoords.top
+    }
+}
+let getOffsetCoords = (mouse, rect) => {
+    return {
+        x: mouse.x - rect.x,
+        y: mouse.y - rect.y
+    }
+}
+
+let cursorInRect = (clickCoord, ansCood) => {
+    let xLine = clickCoord.x > ansCood.x - ansCood.radius && clickCoord.x < ansCood.x + ansCood.radius
+    let yLine = clickCoord.y > ansCood.y - ansCood.radius && clickCoord.y < ansCood.y + ansCood.radius
+
+    return xLine && yLine
+}
+
+
+canvas.addEventListener('mousedown', e => {
+    for (let i = 0; i < answerBlocks.length; i++) {
+        let el = answerBlocks[i];
+        let clickedCoor = {
+            x: e.x - canvas.offsetLeft,
+            y: e.y - canvas.offsetTop
+        }
+        let elCoord = {
+            x: el.x,
+            y: el.y,
+            radius: el.radius
+        }
+        if (cursorInRect(clickedCoor, elCoord)) {
+            el.color = 'red';            
+            el.selected = true;
+        }
+        el.color = '#00FF00';
+    }
+})
+
+canvas.addEventListener('mousemove', e => {
+    for (let i = 0; i < answerBlocks.length; i++) {
+        if (answerBlocks[i].selected) {
+            answerBlocks[i].x = e.x - canvas.offsetLeft;
+            answerBlocks[i].y = e.y - canvas.offsetTop;
+        }
+    }
+})
+
+canvas.addEventListener('mouseup', e => {
+    for (let i = 0; i < answerBlocks.length; i++) {
+        answerBlocks[i].selected = false;
+    }
+})
+
+
+function animate() {
+    ctx.clearRect(0, 0, w, ctx.canvas.height)
+    ctx.fillStyle = 'white'
+    answerBlocks.forEach(e => {
+        e.draw(ctx)
+    })
+    window.requestAnimationFrame(animate)
+}
+
+animate()
 
